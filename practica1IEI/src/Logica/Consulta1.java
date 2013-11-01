254cdd86b5497b89955e59b771cd534cd9a9031f
alramlo <alramlo@inf.upv.es>
2013-11-01 16:39:07 +0100

    primera consulta medio terminada

--- practica1IEI/src/Logica/Consulta1.java
+++ practica1IEI/src/Logica/Consulta1.java
@@ -6,20 +6,41 @@ import java.sql.Statement;
 
 
 public class Consulta1 extends Conexion{
-	Statement s; ResultSet rs = null;
-	public ResultSet transaciones() throws SQLException{
+	Statement s; 
+	ResultSet rs = null;
+	String consulta ="";
+	public void transacionesAux() throws SQLException{
 		
 		try{
+			//LLenado de transacciones desde db1
+			System.out.println("Transacciones de db1");
 			s=this.mysql.createStatement();
-			rs = s.executeQuery("SELECT NumTarjeta, Importe, EmisorTarjeta FROM db1.transacciones");
-			return rs;
-			//while(rs.next()){
+			s.executeUpdate("DELETE FROM mydb.transaccionesaux");
+			s.executeUpdate("INSERT INTO mydb.transaccionesaux (tarjeta, importe, emisor) SELECT NumTarjeta, Importe, EmisorTarjeta FROM db1.transacciones");
+			
+			//LLenado de transacciones desde derby
+			System.out.println("Transacciones de derby");
+			s=this.derby.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+			rs=s.executeQuery("SELECT NumeroTarjeta, Importe, EmisorTarjeta FROM APP.TRANSACCIONES");
+			consulta="INSERT INTO mydb.transaccionesaux (tarjeta, importe, emisor) VALUES ";
+			System.out.println("Creación de consulta Derby");
+
+			while(rs.next())
+			{
 				
-			//}
-		}
-		catch(SQLException e){
-			System.out.println("Error: "+e);
-			return null;
+				if(rs.isLast())
+					consulta=consulta+"('"+rs.getString(1)+"', "+rs.getFloat(2)+", '"+rs.getString(3)+"')";
+				else
+					consulta=consulta+"('"+rs.getString(1)+"', "+rs.getFloat(2)+", '"+rs.getString(3)+"'),";
+
+			}
+			System.out.println("Consulta --> "+consulta);
+			System.out.println("Llenado desde Derby");
+			s=this.mysql.createStatement();
+			s.executeUpdate(consulta);
+			
+		}catch(SQLException e){
+			System.out.println("Error en Consulta1.transacionesAux(): "+e);
 		}
 		
 	}
--- practica1IEI/src/Logica/TestLogica.java
+++ practica1IEI/src/Logica/TestLogica.java
@@ -10,12 +10,8 @@ public class TestLogica {
 		ResultSet rs =null;
 		System.out.println("------------ Empezamos la prueba----------");
 		Consulta1 consulta1 = new Consulta1();
-		rs=consulta1.transaciones();
-		System.out.println("Transacciones:");
-		while(rs.next()){
-			System.out.println("Id: "+rs.getString(1));
-			
-		}
+		consulta1.transacionesAux();
+		System.out.println("Ejecutado proceso de llenado de TransaccionesAux.");
 		System.out.println("------------ Terminamos la prueba----------");
 
 	}
