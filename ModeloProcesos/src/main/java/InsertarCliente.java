5cc565025ae6bd2ca7a64929e3520c93bc7c158a
alderin <alexsp92@gmail.com>
2014-01-08 18:54:17 +0100

    Añadido el nuevo diagrama y las librerias

---
+++ ModeloProcesos/src/main/java/InsertarCliente.java
@@ -0,0 +1,12 @@
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+
+public class InsertarCliente implements org.activiti.engine.delegate.JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
