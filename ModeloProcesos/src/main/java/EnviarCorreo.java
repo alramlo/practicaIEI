bbaf0c3fc00d641199dda1f3984b1f9d9e0efaf4
alramlo <alramlo@inf.upv.es>
2014-01-09 17:45:01 +0100

    Oleeeeeeee

--- ModeloProcesos/src/main/java/EnviarCorreo.java
+++ ModeloProcesos/src/main/java/EnviarCorreo.java
@@ -1,13 +1,12 @@
 import org.activiti.engine.delegate.DelegateExecution;
 
 
-public class calcularPrecio implements org.activiti.engine.delegate.JavaDelegate{
+public class EnviarCorreo implements org.activiti.engine.delegate.JavaDelegate{
 
 	@Override
 	public void execute(DelegateExecution arg0) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("Clase --> calcularPrecio");
-		
+		System.out.println("Clase --> EnviarCorreo");
 	}
 
 }
