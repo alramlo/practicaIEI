4ee2d51f4f70075a7a38ea5c91da4ffc2a2aaba5
alramlo <alramlo@inf.upv.es>
2014-01-09 18:04:04 +0100

    Todo

---
+++ ModeloProcesos/src/main/java/GuardarDatosDenegacion.java
@@ -0,0 +1,12 @@
+import org.activiti.engine.delegate.DelegateExecution;
+
+
+public class GuardarDatosDenegacion implements org.activiti.engine.delegate.JavaDelegate{
+
+	@Override
+	public void execute(DelegateExecution arg0) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Clase --> GuardarDatosDenegacion");
+	}
+
+}
